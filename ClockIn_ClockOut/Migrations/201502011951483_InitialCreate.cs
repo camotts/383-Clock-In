621610204512d9ab9875b1e1256e1fd6daff406d
bgibbs <beau.gibbs@selu.edu>
2015-02-02 00:28:05 -0600

    Seeded database with admin role, but also added 3 other users for test purposes. Used both Envoc clients and a SCRUM master as test subjects.

---
+++ ClockIn_ClockOut/Migrations/201502011951483_InitialCreate.cs
@@ -0,0 +1,52 @@
+namespace ClockIn_ClockOut.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.TimeEntries",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        TimeIn = c.DateTime(nullable: false),
+                        TimeOut = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        Password = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Role = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.TimeEntries");
+            DropTable("dbo.Roles");
+        }
+    }
+}
---
+++ ClockIn_ClockOut/Migrations/Configuration.cs
@@ -0,0 +1,47 @@
+namespace ClockIn_ClockOut.Migrations
+{
+    using ClockIn_ClockOut.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ClockIn_ClockOut.Models.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ClockIn_ClockOut.Models.DatabaseContext";
+        }
+
+        protected override void Seed(ClockIn_ClockOut.Models.DatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            IList<User> defaultUser = new List<User>();
+
+            defaultUser.Add(new User() { ID = 1, Username = "Admin", FirstName = "Group", LastName = "Six", Password = "selu2014", Role = 1 });
+            defaultUser.Add(new User() { ID = 2, Username = "bcornett", FirstName = "Brandon", LastName = "Cornett", Password = "Envoc1", Role = 0 });
+            defaultUser.Add(new User() { ID = 3, Username = "kjoiner", FirstName = "Kyle", LastName = "Joiner", Password = "Envoc2", Role = 0 });
+            defaultUser.Add(new User() { ID = 4, Username = "WiiKingJoe", FirstName = "Joe", LastName = "Naquin", Password = "1Hefebeer", Role = 0 });
+
+            foreach (User std in defaultUser)
+                context.Users.AddOrUpdate(std);
+            context.SaveChanges();
+
+
+        }
+    }
+}
