bc8d2ab558bb3b2d0a840065104e36e6d0043aad
w0474732 <patrick.dolan@selu.edu>
2015-02-03 22:34:25 -0600

    Made the Index pages now have buttons instead of links;
    Put in links for the control pages;
    
    Theme properly works, and fixed a bug with it throwing an error occasionally;

--- ClockIn_ClockOut/Scripts/Custom Scripts/bootswatch.js
+++ ClockIn_ClockOut/Scripts/Custom Scripts/bootswatch.js
@@ -1,38 +1,73 @@
 ﻿
-var theme = JSON.parse(localStorage.getItem("savedTheme"));
-var pageLoad = false;
+try{
+    var theme = JSON.parse(localStorage.getItem("savedTheme"));
+    var pageLoad = false;
+} catch (ex) {
+    var theme = null;
+    var pageload = true;
+}
 
+
+//Initial load of the api
 $.get("http://api.bootswatch.com/3/", function (data) {
     var themes = data.themes;
     var select = $("select");
 
-    select.show();
-   
     themes.forEach(function (value, index) {
         select.append($("<option />")
                 .val(index)
                 .text(value.name));
     });
 
-        select.change(function () {
-        if (pageLoad) {
+    //put the default value of the theme if the theme is null
+    if (theme == null) {
+        document.getElementById("API-DropDown").selectedIndex = "6";
+    }
+
+    select.change(function () {
+        if (pageLoad)
+        {
             var theme1 = themes[$(this).val()];
             localStorage.setItem("savedTheme", JSON.stringify(theme1));
             $("link").attr("href", theme1.css);
         }
-        else {
+        else
+        {
             pageLoad = true;
-            console.log(theme.css);
             $("link").attr("href", theme.css);
         }
     }).change();
 
-}, "json").fail(function () {
 
-});
+
+}, "json")
+
 
 //For the buttons to change the themes
 function change(id) {
-    console.log("Hit");
     document.getElementById("API-DropDown").selectedIndex = id;
+
+    $.get("http://api.bootswatch.com/3/", function (data) {
+        var themes = data.themes;
+        var select = $("select");
+
+        select.change(function () {
+            if (pageLoad) {
+                var theme1 = themes[$(this).val()];
+                localStorage.setItem("savedTheme", JSON.stringify(theme1));
+                $("link").attr("href", theme1.css);
+            }
+            else {
+                pageLoad = true;
+                $("link").attr("href", theme.css);
+            }
+        }).change();
+
+        themes.forEach(function (value, index) {
+            select.append($("<option />")
+                    .val(index)
+                    .text(value.name));
+        });
+
+    }, "json")
 }
